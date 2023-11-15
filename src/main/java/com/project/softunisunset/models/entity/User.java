7d1c15f47732a0ba258167b34f1b43c5c37f7fec
bibidobreva <114009406+bibidobreva@users.noreply.github.com>
2023-11-15 23:30:56 +0200

    Update User.java

--- src/main/java/com/project/softunisunset/models/entity/User.java
+++ src/main/java/com/project/softunisunset/models/entity/User.java
@@ -39,4 +39,11 @@ public class User extends BaseEntity {
 
     public User(){}
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
