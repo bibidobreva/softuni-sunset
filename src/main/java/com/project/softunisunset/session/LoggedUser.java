2827ac2fe476bca5dd1d92edf7170479a9869104
bibidobreva <114009406+bibidobreva@users.noreply.github.com>
2023-11-23 13:37:13 +0200

    new class

---
+++ src/main/java/com/project/softunisunset/session/LoggedUser.java
@@ -0,0 +1,27 @@
+package com.project.softunisunset.session;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class LoggedUser {
+    private long id;
+    private String username;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
