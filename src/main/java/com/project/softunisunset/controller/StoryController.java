1ef39223fad8a9c74b149149032ecca3f069acd5
bibidobreva <114009406+bibidobreva@users.noreply.github.com>
2023-11-23 14:00:15 +0200

    new classes and repositories

---
+++ src/main/java/com/project/softunisunset/controller/StoryController.java
@@ -0,0 +1,7 @@
+package com.project.softunisunset.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class StoryController {
+}
---
+++ src/main/java/com/project/softunisunset/service/StoryService.java
@@ -0,0 +1,14 @@
+package com.project.softunisunset.service;
+
+import com.project.softunisunset.repositories.StoryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StoryService {
+    private final StoryRepository storyRepository;
+
+
+    public StoryService(StoryRepository storyRepository) {
+        this.storyRepository = storyRepository;
+    }
+}
