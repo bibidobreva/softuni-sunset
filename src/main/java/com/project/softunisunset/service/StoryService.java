4bf67db154656ac5ed1905e69424a6d480df9246
bibidobreva <114009406+bibidobreva@users.noreply.github.com>
2023-11-23 15:08:42 +0200

    Service update on create method

--- src/main/java/com/project/softunisunset/service/StoryService.java
+++ src/main/java/com/project/softunisunset/service/StoryService.java
@@ -1,17 +1,31 @@
 package com.project.softunisunset.service;
 
+import com.project.softunisunset.models.dto.CreateStoryDTO;
+import com.project.softunisunset.models.entity.Story;
 import com.project.softunisunset.repositories.StoryRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StoryService {
     private final StoryRepository storyRepository;
+    private final ModelMapper modelMapper;
 
 
-    public StoryService(StoryRepository storyRepository) {
+    public StoryService(StoryRepository storyRepository, ModelMapper modelMapper) {
         this.storyRepository = storyRepository;
+        this.modelMapper = modelMapper;
     }
 
 
-    
+    public boolean create(CreateStoryDTO createStoryDTO){
+        Story story = new Story();
+
+        //to check user
+
+
+        this.storyRepository.save(this.modelMapper.map(createStoryDTO, Story.class));
+
+        return true;
+    }
 }
