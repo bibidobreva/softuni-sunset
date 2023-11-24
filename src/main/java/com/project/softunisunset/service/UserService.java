b57451717d1de7b0256979ffeb486fe1a3376929
bibidobreva <114009406+bibidobreva@users.noreply.github.com>
2023-11-24 20:26:53 +0200

    UserService

---
+++ src/main/java/com/project/softunisunset/service/UserService.java
@@ -0,0 +1,19 @@
+package com.project.softunisunset.service;
+
+import com.project.softunisunset.repositories.StoryRepository;
+import com.project.softunisunset.repositories.SunsetRepository;
+import com.project.softunisunset.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final SunsetRepository sunsetRepository;
+    private final StoryRepository storyRepository;
+
+    public UserService(UserRepository userRepository, SunsetRepository sunsetRepository, StoryRepository storyRepository) {
+        this.userRepository = userRepository;
+        this.sunsetRepository = sunsetRepository;
+        this.storyRepository = storyRepository;
+    }
+}
