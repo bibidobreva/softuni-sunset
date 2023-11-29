30dd0606984649ca409ac9a744ca6870af711922
bibidobreva <114009406+bibidobreva@users.noreply.github.com>
2023-11-29 13:42:36 +0200

    Home feed

--- src/main/java/com/project/softunisunset/controller/HomeController.java
+++ src/main/java/com/project/softunisunset/controller/HomeController.java
@@ -1,13 +1,49 @@
 package com.project.softunisunset.controller;
 
+import com.project.softunisunset.models.entity.Sunset;
+import com.project.softunisunset.service.SunsetService;
 import com.project.softunisunset.session.LoggedUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
     private LoggedUser loggedUser;
+    private SunsetService sunsetService;
+
 
     public HomeController(LoggedUser loggedUser) {
         this.loggedUser = loggedUser;
+    }
+
+    @GetMapping("/")
+    public String loggedOutIndex(){
+
+        //TODO
+        long loggedUserId = this.loggedUser.getId();
+
+
+        if(loggedUserId!=0){
+            return "redirect:/home";
+        }
+        return "index";
+    }
+
+
+    @GetMapping("/home")
+    public String LoggedIndex(Model model){
+        long loggedUserId = this.loggedUser.getId();
+        if(loggedUserId==0){
+            return "redirect:/";
+        }
+
+        List<Sunset> sunsets = this.sunsetService.getAllSunsets();
+
+        model.addAttribute("feedSunsets",sunsets);
+
+        return "home";
     }
 }
