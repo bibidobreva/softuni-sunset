38dc0eb80ddf725bec218a39eb19d374ceb54f97
bibidobreva <114009406+bibidobreva@users.noreply.github.com>
2023-12-14 17:23:20 +0200

    UserDetailsService tests

--- src/test/java/com/project/softunisunset/service/SunsetUserDetailsServiceTest.java
+++ src/test/java/com/project/softunisunset/service/SunsetUserDetailsServiceTest.java
@@ -1,11 +1,21 @@
 package com.project.softunisunset.service;
 
+import com.project.softunisunset.models.entity.User;
+import com.project.softunisunset.models.entity.UserRoleEntity;
+import com.project.softunisunset.models.enums.UserRolesEnums;
 import com.project.softunisunset.repositories.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class SunsetUserDetailsServiceTest {
@@ -22,5 +32,55 @@ class SunsetUserDetailsServiceTest {
     @Test
     void testMock(){
 
+        User user = new User();
+        user.setFirstName("Anna");
+        when(mockUserRepository.findByUsername("example"))
+                .thenReturn(Optional.of(user));
+
+        Optional<User> userOpt = mockUserRepository.findByUsername("example");
+
+        User userOption = userOpt.get();
+        Assertions.assertEquals("Anna",userOption.getFirstName() );
+    }
+
+    @Test
+    void userNotFound(){
+        Assertions.assertThrows(UsernameNotFoundException.class,
+                ()->serviceToTest.loadUserByUsername("test"));
+
+    }
+
+    @Test
+    void  userFoundException(){
+
+        User testUser = createUser();
+        when(mockUserRepository.findByUsername(testUser.getUsername()))
+                .thenReturn(Optional.of(testUser));
+
+        UserDetails userDetails = serviceToTest.loadUserByUsername(testUser.getUsername());
+
+        Assertions.assertNotNull(userDetails);
+        Assertions.assertEquals(testUser.getUsername(),userDetails.getUsername());
+        Assertions.assertEquals(testUser.getPassword(), userDetails.getPassword());
+        Assertions.assertEquals(1, userDetails.getAuthorities().size());
+
+    }
+
+
+    private static User createUser(){
+        User user = new User();
+        user.setUsername("test");
+        user.setFirstName("testName");
+        user.setLastName("lastName");
+        user.setEmail("test@email.com");
+        user.setPassword("passwordTest");
+
+        UserRoleEntity role = new UserRoleEntity();
+        UserRolesEnums admin = UserRolesEnums.ADMIN;
+        role.setRole(admin);
+
+
+        user.setRoles(List.of(role));
+        return user;
     }
 }
